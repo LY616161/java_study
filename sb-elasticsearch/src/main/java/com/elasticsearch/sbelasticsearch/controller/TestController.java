b8e53c11f58ff013c3150d2408353225520ace1a
616161 <269496579@qq.com>
2019-01-09 20:56:05 +0800

    ElasticsearchRepository自带的搜索功能

--- sb-elasticsearch/src/main/java/com/elasticsearch/sbelasticsearch/controller/TestController.java
+++ sb-elasticsearch/src/main/java/com/elasticsearch/sbelasticsearch/controller/TestController.java
@@ -3,8 +3,13 @@ package com.elasticsearch.sbelasticsearch.controller;
 import com.elasticsearch.sbelasticsearch.entity.Movie;
 import com.elasticsearch.sbelasticsearch.mapper.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class TestController {
@@ -20,5 +25,21 @@ public class TestController {
     @GetMapping("/query/{id}")
     public Movie query(@PathVariable("id") long id){
         return movieRepository.queryMovieById(id);
+    }
+
+    @GetMapping("/searchByName")
+    public Page<Movie> searchByName(@RequestParam("name") String name,
+                             @RequestParam("pageIndex") int pageIndex,
+                             @RequestParam("pageSize") int pageSize) {
+        Pageable pageable = PageRequest.of(pageIndex, pageSize);
+        return movieRepository.findByMovieNameLike(name,pageable);
+    }
+
+    @GetMapping("/searchByNameOrDirector")
+    public Page<Movie> searchByNameOrDirector(@RequestParam("name") String name,
+                             @RequestParam("pageIndex") int pageIndex,
+                             @RequestParam("pageSize") int pageSize) {
+        Pageable pageable = PageRequest.of(pageIndex, pageSize);
+        return movieRepository.findByMovieNameLikeOrDirectorLike(name,name,pageable);
     }
 }
--- sb-elasticsearch/src/main/java/com/elasticsearch/sbelasticsearch/mapper/MovieRepository.java
+++ sb-elasticsearch/src/main/java/com/elasticsearch/sbelasticsearch/mapper/MovieRepository.java
@@ -1,10 +1,14 @@
 package com.elasticsearch.sbelasticsearch.mapper;
 
 import com.elasticsearch.sbelasticsearch.entity.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 public interface MovieRepository extends ElasticsearchRepository<Movie,Long> {
     Movie queryMovieById(long id);
+    Page<Movie> findByMovieNameLikeOrDirectorLike(String movieName, String director, Pageable pageable);
+    Page<Movie> findByMovieNameLike(String movieName,Pageable pageable);
 }
